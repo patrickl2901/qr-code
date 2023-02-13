ad8b245b55e7cd042ced7342755dc2aedb5e021c
patrickl2901 <patrickloos@gmx.net>
2023-02-13 16:50:31 +0100

    .

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ function App() {
   return (
     <div className="App">
       <Qr />
-      Improve your front-end skills by building projects
-      Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
       <div class="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
         Coded by <a href="https://github.com/patrickl2901">patrickl2901</a>.
--- src/Qr.jsx
+++ src/Qr.jsx
@@ -6,8 +6,10 @@ function Qr() {
   return (
     <div className='qr'>
         <img src={qr} alt='qr' className='qr-img' />
-        <h1 className='title'>Improve your front-end skills by building projects</h1>
-        <p className='text'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        <div className='text-container'>
+            <h1 className='title'>Improve your front-end skills by building projects</h1>
+            <p className='text'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div>
     </div>
   )
 }
